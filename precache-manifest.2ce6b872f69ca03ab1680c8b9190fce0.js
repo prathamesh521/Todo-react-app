1cb758b530519c56139b6195d15154a8f0ccd77b
prathamesh521 <prathameshkokate9@gmail.com>
2021-05-12 13:22:32 +0530

    Updates

---
+++ precache-manifest.2ce6b872f69ca03ab1680c8b9190fce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cb527dfabf1bcd1353c4fbfa5a41b8",
+    "url": "/Todo-react-app/index.html"
+  },
+  {
+    "revision": "242857b562c20dae1b8c",
+    "url": "/Todo-react-app/static/css/main.b046be49.chunk.css"
+  },
+  {
+    "revision": "9b9b2e5e52965227eb4f",
+    "url": "/Todo-react-app/static/js/2.3997933f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-react-app/static/js/2.3997933f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242857b562c20dae1b8c",
+    "url": "/Todo-react-app/static/js/main.b8e88aeb.chunk.js"
+  },
+  {
+    "revision": "51226b0d2ad477ad9382",
+    "url": "/Todo-react-app/static/js/runtime-main.305f9ecc.js"
+  }
+]);
